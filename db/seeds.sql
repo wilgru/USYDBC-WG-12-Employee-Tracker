9d0002d014284f7c3d201233939be829eb37f426
wilgru <william.gruszka@gmail.com>
2022-05-16 13:08:21 +1000

    added better data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO departments (name)
-VALUES ("retail");
+VALUES ("Retail");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("customer service", 100, 1),
-       ("manager", 200, 1);
+VALUES ("Customer Service", 40000, 1),
+       ("Manager", 80000, 1);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("john", "doe", 2, NULL),
+VALUES ("john", "D", 2, NULL),
        ("William", "G", 1, 1),
        ("Jack", "H", 1, 1);
